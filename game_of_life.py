25ec6a2bd05e1d924a11312f1da7c65207aa8e8c
Axl-Mont <montenegroaxel@outlook.com>
2023-10-16 23:05:00 -0300

    Se dibuja el tablero en el display

--- game_of_life.py
+++ game_of_life.py
@@ -3,11 +3,26 @@ import numpy as np
 
 pygame.init()
 
-width, height = 1000, 1000
-screen = pygame.display.set_mode((height,width))
-background = 25, 25, 25
+width, height = 800, 800
+screen = pygame.display.set_mode((width, height))
+
+background = (25, 25, 25)
 screen.fill(background)
 
-while True:
-    pass
+celdaX, celdaY = 25, 25
 
+celdaWidth = width // celdaX
+celdaHeight = height // celdaY
+
+while True:
+    for y in range(0, celdaY):
+        for x in range(0, celdaX):
+
+            polygono = [((x)* celdaWidth, y * celdaHeight),
+                        ((x + 1) * celdaWidth, y * celdaHeight),
+                        ((x + 1) * celdaWidth, (y + 1) * celdaHeight),
+                        ((x) * celdaWidth, (y + 1) * celdaHeight)]
+
+            pygame.draw.polygon(screen, (128, 128, 128), polygono, 1)
+            
+    pygame.display.flip()
