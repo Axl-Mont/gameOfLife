f3e3728cdb744c7de5bead1a8c7bf699d7745e7f
Axl-Mont <montenegroaxel@outlook.com>
2023-10-19 20:12:01 -0300

    se completo la logica y se agrego un caso de prueba

--- game_of_life.py
+++ game_of_life.py
@@ -1,28 +1,69 @@
 import pygame
 import numpy as np
+import time
 
 pygame.init()
 
-width, height = 800, 800
+width, height = 700, 700
 screen = pygame.display.set_mode((width, height))
 
 background = (25, 25, 25)
 screen.fill(background)
 
-celdaX, celdaY = 25, 25
+
+celdaX, celdaY = 50, 50
 
 celdaWidth = width // celdaX
 celdaHeight = height // celdaY
 
-while True:
-    for y in range(0, celdaY):
-        for x in range(0, celdaX):
+estadoCelda = np.zeros((celdaY, celdaX))
 
+estadoCelda[21,21] = 1
+estadoCelda[22,22] = 1
+estadoCelda[22,23] = 1
+estadoCelda[21,23] = 1
+estadoCelda[20,23] = 1
+
+while True:
+    
+    nuevoEstado = np.copy(estadoCelda)
+    
+    screen.fill(background)
+    
+    time.sleep(0.2)
+    
+    for y in range(0, celdaY):
+        
+        for x in range(0, celdaX):
+            
+            #Calcula las 8 posiciones de las celdas vecinas
+            celdaVecina = estadoCelda[(x-1) % celdaX, (y-1) % celdaY] + \
+                          estadoCelda[(x  ) % celdaX, (y-1) % celdaY] + \
+                          estadoCelda[(x+1) % celdaX, (y-1) % celdaY] + \
+                          estadoCelda[(x-1) % celdaX, (y  ) % celdaY] + \
+                          estadoCelda[(x+1) % celdaX, (y  ) % celdaY] + \
+                          estadoCelda[(x-1) % celdaX, (y+1) % celdaY] + \
+                          estadoCelda[(x  ) % celdaX, (y+1) % celdaY] + \
+                          estadoCelda[(x+1) % celdaX, (y+1) % celdaY]
+                          
+            #Regla 1: Una celula muerta con exactamente 3 vecinas vivas, Revive!
+            if estadoCelda[x,y] == 0 and celdaVecina == 3:
+                nuevoEstado[x,y] = 1
+            #Regla 2: Una celula viva con menos de 2 o mas de 3 vecinas vivas, Muere!
+            elif estadoCelda[x,y] == 1 and (celdaVecina < 2 or celdaVecina > 3):
+                nuevoEstado[x,y] = 0
+                
+            # poligono de cada celda que se dibuja
             polygono = [((x)* celdaWidth, y * celdaHeight),
                         ((x + 1) * celdaWidth, y * celdaHeight),
                         ((x + 1) * celdaWidth, (y + 1) * celdaHeight),
                         ((x) * celdaWidth, (y + 1) * celdaHeight)]
 
-            pygame.draw.polygon(screen, (128, 128, 128), polygono, 1)
-            
+            if nuevoEstado[x,y] == 0:
+                pygame.draw.polygon(screen, (128, 128, 128), polygono, 1)
+            else:
+                pygame.draw.polygon(screen, (255, 255, 255), polygono, 0)
+                
+    estadoCelda = np.copy(nuevoEstado)
+
     pygame.display.flip()
